57a622cb360b2a67545b0a756e315c63402508a7
EnDataTrex <isabellejohansson.0004@gmail.com>
2025-05-11 13:56:54 +0200

    Skapde ändringar som gör det möjligt för händelser att ske när man klickar på knapparna + lade till några kommentarer

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -21,32 +21,63 @@ public class Gui extends Application {
 
     MenuButton menuBar = new MenuButton("File"); //till för menyknappar
     MenuItem newMapmenu = new MenuItem("New Map");
+    newMapmenu.setOnAction(event -> {});//Skapar ett event med vad som ska hända när man klickar på knappen (Kan behöva ändas beroende på vad action är)
+
     MenuItem openInMenu = new MenuItem("Open");
+    newMapmenu.setOnAction(event -> {});
+
     MenuItem saveInMenu = new MenuItem("Save");
+    saveInMenu.setOnAction(event -> {});
+
     MenuItem saveImageInMenu = new MenuItem("Save Image");
+    saveImageInMenu.setOnAction(event -> {});
+
     MenuItem exitInMenu = new MenuItem("Exit");
-    menuBar.getItems().addAll(newMapmenu, openInMenu, saveInMenu, saveImageInMenu, exitInMenu);
+    exitInMenu.setOnAction(event -> {});
+
+    menuBar.getItems().addAll(newMapmenu, openInMenu, saveInMenu, saveImageInMenu, exitInMenu); //Lägger till alla knappar i menyn
 
     root.setBackground(Background.fill(Color.LIGHTGREY)); //sätter bakgrund
     root.add(menuBar, 0, 0); //lägger till menubar i roten
     root.add(hbox, 0,1); //lägger till hbox i roten
-    root.setPadding(new Insets(10));
+    root.setPadding(new Insets(10)); //sätter padding aka utrymme mellan rotens innehåll och kanter
 
-    hbox.setBackground(Background.fill(Color.LIGHTBLUE));
-    hbox.setAlignment(Pos.CENTER);
-    hbox.setSpacing(3);
+    hbox.setBackground(Background.fill(Color.LIGHTBLUE)); //sätter "övriga knappars" bakgrund till blå
+    hbox.setAlignment(Pos.CENTER); //centrerar de "övriga knapparna"
+    hbox.setSpacing(3); //Sätter ett utrymme runt knapparna
 
-    GridPane.setMargin(menuBar, new Insets(0,0,10,0));
+    GridPane.setMargin(menuBar, new Insets(0,0,10,0)); //sätter utrymme mellan boxar i griden
 
-    ColumnConstraints column1 = new ColumnConstraints();
-    column1.setHgrow(Priority.ALWAYS);
-    root.getColumnConstraints().add(column1);
+    ColumnConstraints column1 = new ColumnConstraints(); //skapar kolumnernas constraints
+    column1.setHgrow(Priority.ALWAYS); //Låter kolumnens längd växa dynamiskt
+    root.getColumnConstraints().add(column1); //Lägger till det i roten
 
-    ArrayList<String> elements = new ArrayList<>(Arrays.asList("Find Path", "Show Connection", "New Place", "New Connection", "Change Connection"));
+    ArrayList<String> elements = new ArrayList<>(Arrays.asList("Find Path", "Show Connection", "New Place", "New Connection", "Change Connection")); //Skapar lista med övriga knappar
     for (String element : elements) {
-      Button button = new Button(element);
-      hbox.getChildren().add(button);
+      Button button = new Button(element);//skapar ny knapp
+      button.setBackground(Background.fill(Color.FLORALWHITE)); //sätter färg
+      hbox.getChildren().add(button); //lägger till den som ett barn i hbox
     }
+
+    hbox.getChildren().get(0).onMouseClickedProperty().setValue(event -> {
+      hbox.setBackground(Background.fill(Color.LIGHTPINK)); //skapar ett event för första knappen (ändrar färg)
+    });
+
+    hbox.getChildren().get(1).onMouseClickedProperty().setValue(event -> {
+      hbox.setBackground(Background.fill(Color.LIGHTBLUE));
+    });
+
+    hbox.getChildren().get(2).onMouseClickedProperty().setValue(event -> {
+      hbox.setBackground(Background.fill(Color.LIGHTYELLOW));
+    });
+
+    hbox.getChildren().get(3).onMouseClickedProperty().setValue(event -> {
+      hbox.setBackground(Background.fill(Color.PURPLE));
+    });
+
+    hbox.getChildren().get(4).onMouseClickedProperty().setValue(event -> {
+      hbox.setBackground(Background.fill(Color.BLACK));
+    });
 
     Scene scene = new Scene(root);
     stage.setScene(scene);

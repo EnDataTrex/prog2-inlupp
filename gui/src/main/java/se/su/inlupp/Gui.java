cff108d3571d007caca65ba5a78a76b9a82466b2
EnDataTrex <isabellejohansson.0004@gmail.com>
2025-05-11 13:33:21 +0200

    Lade till så knapparna är rörliga

--- gui/src/main/java/se/su/inlupp/Gui.java
+++ gui/src/main/java/se/su/inlupp/Gui.java
@@ -9,7 +9,6 @@ import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -19,18 +18,29 @@ public class Gui extends Application {
     Graph<String> graph = new ListGraph<String>();
     GridPane root = new GridPane(); //roten
     HBox hbox = new HBox(); // till för "övriga" knappar
+
     MenuButton menuBar = new MenuButton("File"); //till för menyknappar
-    MenuItem fileMenu = new MenuItem("Hej");
-    MenuItem newMapMenu = new MenuItem("New Map");
-    menuBar.getItems().addAll(fileMenu, newMapMenu);
-    root.setBackground(Background.fill(Color.LIGHTBLUE)); //sätter bakgrund
+    MenuItem newMapmenu = new MenuItem("New Map");
+    MenuItem openInMenu = new MenuItem("Open");
+    MenuItem saveInMenu = new MenuItem("Save");
+    MenuItem saveImageInMenu = new MenuItem("Save Image");
+    MenuItem exitInMenu = new MenuItem("Exit");
+    menuBar.getItems().addAll(newMapmenu, openInMenu, saveInMenu, saveImageInMenu, exitInMenu);
+
+    root.setBackground(Background.fill(Color.LIGHTGREY)); //sätter bakgrund
     root.add(menuBar, 0, 0); //lägger till menubar i roten
     root.add(hbox, 0,1); //lägger till hbox i roten
-    hbox.setAlignment(Pos.CENTER);
     root.setPadding(new Insets(10));
+
+    hbox.setBackground(Background.fill(Color.LIGHTBLUE));
+    hbox.setAlignment(Pos.CENTER);
     hbox.setSpacing(3);
+
     GridPane.setMargin(menuBar, new Insets(0,0,10,0));
 
+    ColumnConstraints column1 = new ColumnConstraints();
+    column1.setHgrow(Priority.ALWAYS);
+    root.getColumnConstraints().add(column1);
 
     ArrayList<String> elements = new ArrayList<>(Arrays.asList("Find Path", "Show Connection", "New Place", "New Connection", "Change Connection"));
     for (String element : elements) {

218ee9516957909fba70ac817aa2807ded5030fe
Maja Johnsson <maja.i.johnsson@gmail.com>
2025-05-16 16:09:03 +0200

    la till location

---
+++ gui/src/main/java/se/su/inlupp/Location.java
@@ -0,0 +1,24 @@
+package se.su.inlupp;
+
+public class Location {
+    private final String name;
+    private final double x, y;
+
+    public Location(String name, double x, double y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public String toString() {
+        return name + " (" + x + ", " + y + ")";
+    }
+}
